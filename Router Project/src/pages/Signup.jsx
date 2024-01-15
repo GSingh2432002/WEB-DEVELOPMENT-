ef1c7244f103321408c40847273bdb00a8e6d574
GSingh2432002 <gsingh332211@gmail.com>
2024-01-15 21:23:22 +0530

    Update

--- Router Project/src/pages/Signup.jsx
+++ Router Project/src/pages/Signup.jsx
@@ -1,17 +1,18 @@
-import Template from "../components/Auth/Template";
-import signupImg from "../assets/signup.png";
+import React from 'react'
+import signupImg from "../assets/signup.png"
+import Template from '../Components/Template'
 
-function Signup({ setIsLoggedIn }) {
+const Signup = ({setIsLoggedIn}) => {
   return (
     <Template
       title="Join the millions learning to code with StudyNotion for free"
-      description1="Build skills for today, tomorrow, and beyond."
-      description2="Education to future-proof your career."
+      desc1="Build skills for today, tomorrow, and beyond."
+      desc2="Education to future-proof your career."
       image={signupImg}
-      formType="signup"
+      formtype="signup"
       setIsLoggedIn={setIsLoggedIn}
     />
-  );
+  )
 }
 
-export default Signup;
+export default Signup

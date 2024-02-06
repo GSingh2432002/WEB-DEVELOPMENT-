8eeb654f4f37188e0be7a4ce63e6a808966b6b64
GSingh2432002 <gsingh332211@gmail.com>
2024-02-06 23:47:34 +0530

    Update

--- BD Auth. CLASS I/controllers/auth.js
+++ BD Auth. CLASS I/controllers/auth.js
@@ -1,5 +1,8 @@
 const bcrypt = require("bcrypt");
 const User = require("../models/User");
+const jwt = require("jsonwebtoken");
+const {options} = require("../routes/user");
+require("dotenv").config();
 
 //Signup route handler
 exports.signup = async(req,res) => {
@@ -41,6 +44,80 @@ exports.signup = async(req,res) => {
         return res.status(500).json({
             success:false,
             message:'User cannot be created at this time',
+        });
+    }
+}
+
+//Login
+exports.login = async (req,res) => {
+    try{
+        //data fetch
+        const {email,password} = req.body;
+        //validation on email and password
+        if(!email || ! password) {
+            return res.statuc(400).json({
+                success:false,
+                message:'Please fill all the details Carefully',
+            });
+        }
+
+        //check for registered user
+        let user = await User.findOne({email});
+        //if not a registered user
+        if(!user){
+            return res.status(401).json({
+                success: false,
+                message:'User is not registered',
+            });
+        }
+        const payload = {
+            email:user.email,
+            id:user._id,
+            role:user.role,
+        };
+
+        //verify password and generate a JWT token
+        if(await bcrypt.compare(password,user.password)) {
+            //password match
+            let token = jwt.sign(payload,
+                process.env.JWT_SECRET,
+                {
+                    expiresIn:"2h",
+                });
+            
+            user = user.toObject();
+            user.token = token;
+            user.password = undefined;
+            const options = {
+                expires: new Date(Date.now() + 300000 ),
+                httpOnly:true,
+            }
+            res.cookie("token",token,options).status(200).json({
+                success:true,
+                token,
+                user,
+                message:'User Logged in Successfully',
+            });
+            // res.status(200).json({
+            //     success:true,
+            //     token,
+            //     user,
+            //     message:'User Logged in Successfully',
+            // });
+        }
+        else{
+            //password do not match
+            return res.status(403).json({
+                success:false,
+                message:"Password Incorrect",
+            });
+        }
+    }
+    catch(error){
+        console.log(error);
+        return res.status(500).json({
+            success: false,
+            message:'login failure',
         });
     }
 }
--- BD Auth. CLASS I/routes/user.js
+++ BD Auth. CLASS I/routes/user.js
@@ -1,9 +1,55 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
-const {login,signup} = require("../controllers/auth");
+const User = require("../models/User");
 
-// router.post("/login",login);
+const {login,signup} = require("../controllers/auth");
+const {Auth,isStudent,isAdmin} = require("../middlewares/Auth");
+
+router.post("/login",login);
 router.post("/signup",signup);
+
+//testing protected routes for single middleware
+router.get("/test",Auth,(req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for TESTS',
+    });
+});
+
+//Protected Route
+router.get("/student",Auth,isStudent,(req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Student',
+    });
+});
+
+router.get("/admin", Auth, isAdmin, (req,res) => {
+    res.json({
+        success:true,
+        message:'Welcome to the Protected route for Admin',
+    });
+});
+
+router.get("/getEmail",Auth,async (req,res)=> {
+    try{
+        const id = req.user.id;
+        const user = await User.findById(id);
+
+        res.status(200).json({
+            success:true,
+            user:user,
+            message:'welcome to the email route',
+        })
+    }
+    catch(error){
+        res.status(500).json({
+            success:false,
+            error:error.message,
+            message:'fat gya code',
+        });
+    }
+});
 
 module.exports = router;
